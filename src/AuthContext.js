df183f7012704bd11cc3d8303f8e0e332b3177ab
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-07-31 18:27:24 +0300

    add refresh token possibility to prenvent sufficient loggin

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,5 +1,19 @@
-import React, { createContext, useState } from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 
+function deleteAllCookies() {
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
+}
+export const getCookie = name => {
+    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
+    if (match) {
+        return decodeURIComponent(match[2]);
+    }
+    return null;
+};
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -11,7 +25,35 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
         setIsLoggedIn(false);
+        deleteAllCookies();
     };
+
+     useEffect(() => {
+    // Optionally, retrieve tokens from localStorage or cookies on app startup
+    const refreshToken = getCookie('refresh-token');
+    const token = getCookie('token');
+    if (refreshToken) {
+      refreshAccessToken(refreshToken,token);
+    }
+  }, []);
+
+     const refreshAccessToken = async (refreshToken,token) => {
+       const body = await  fetch(`${process.env.REACT_APP_LINK_LOCAL}/api/Auth/refresh-tokens`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'refresh-token': refreshToken,
+           'Authorization': `Bearer ${token}`,
+        },
+         credentials: 'include'})
+        if (body.ok) {
+          setIsLoggedIn(true);
+        }
+        else {
+            setIsLoggedIn(false);
+
+        }
+  };
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
--- src/components/pages/UserDetails.jsx
+++ src/components/pages/UserDetails.jsx
@@ -4,8 +4,9 @@ import UserPanel from "../UserPanel";
 import UserDevices from "../UserDevices";
 import React, {useState, useEffect} from 'react';
 import {DeviceDto} from "../../models/DeviceDto";
+import {getCookie} from "../../AuthContext";
 
-export const getCookie = name => document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))?.at(2);
+
 
 
 export default function UserDetails() {
@@ -15,6 +16,7 @@ export default function UserDetails() {
             try {
                 const response = await fetch(`${process.env.REACT_APP_LINK_LOCAL}/api/DevicesDb/DeviceOfUser`, {
                     method: 'GET', headers: {
+
                         'Authorization': `Bearer ${getCookie('token')}`, 'Content-Type': 'application/json',
                     },
                     credentials: 'include',
