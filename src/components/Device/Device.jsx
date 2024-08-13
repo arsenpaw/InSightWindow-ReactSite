68e81f35bbbc8bfbd9c87a24b043e2d04e042ced
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-08-13 23:47:12 +0300

    make separate component for device

---
+++ src/components/Device/Device.jsx
@@ -0,0 +1,45 @@
+import styles from "../UserDevice/UserDevices.module.css";
+import React, {useContext} from "react";
+import {removeDevice} from "../UserDevice/UserDevices";
+import {getCookie} from "../../contexts/AuthContext";
+import {DeviceContext} from "../../contexts/DeviceContext";
+
+export function Device(props) {
+    const { devices, setDevices } = useContext(DeviceContext);
+    const removeDevice = async (deviceId) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_LINK}/api/UsersDb/UnbindFrom?deviceId=${deviceId}`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${getCookie('token')}`,
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+            if (response.status === 200) {
+                setDevices(devices.filter(device => device.id !== deviceId));
+                alert('Device removed successfully');
+            } else {
+                console.error('Error removing device:', response.status);
+            }
+        } catch (error) {
+            console.error('Error removing device:', error);
+        }
+    };
+
+  return (
+      <div>
+          <div  className={styles.deviceItem} style={{position: 'relative'}}>
+              <button
+
+                  onClick={() => removeDevice(props.device.id)}
+                  className={styles.removeButton}
+              >
+                  &times;
+              </button>
+              <p>ID: {props.device.id}</p>
+              <p>Type: {props.device.deviceType}</p>
+          </div>
+      </div>
+  );
+}
--- src/components/UserDevice/UserDevices.jsx
+++ src/components/UserDevice/UserDevices.jsx
@@ -2,30 +2,12 @@ import React, { useContext } from "react";
 import styles from "./UserDevices.module.css"; // Import CSS Module
 import { getCookie } from "../../contexts/AuthContext";
 import { DeviceContext } from "../../contexts/DeviceContext";
+import {Device} from "../Device/Device";
 
 export default function UserDevices() {
-    const { devices, setDevices } = useContext(DeviceContext);
+    const { devices, removeDevice } = useContext(DeviceContext);
 
-    const removeDevice = async (deviceId) => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_LINK}/api/UsersDb/UnbindFrom?deviceId=${deviceId}`, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${getCookie('token')}`,
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-            });
-            if (response.status === 200) {
-                setDevices(devices.filter(device => device.id !== deviceId));
-                alert('Device removed successfully');
-            } else {
-                console.error('Error removing device:', response.status);
-            }
-        } catch (error) {
-            console.error('Error removing device:', error);
-        }
-    };
+
 
     if (devices === null) {
         return (
@@ -42,17 +24,8 @@ export default function UserDevices() {
         <div className={styles.userDevices}>
             <h2>Your Devices</h2>
             <div className={styles.deviceList}>
-                {devices.map((device, key) => (
-                    <div key={key} className={styles.deviceItem} style={{ position: 'relative' }}>
-                        <button
-                            onClick={() => removeDevice(device.id)}
-                            className={styles.removeButton}
-                        >
-                            &times;
-                        </button>
-                        <p>ID: {device.id}</p>
-                        <p>Type: {device.deviceType}</p>
-                    </div>
+                {devices.map((deviceMap, key) => (
+                     <Device key={key} device={deviceMap} />
                 ))}
             </div>
         </div>
