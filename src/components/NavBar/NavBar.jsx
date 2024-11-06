62ab0123ecba53ad8cf8b5bf46054a972a9b0160
Arsen <arsen.shulak@gmail.com>
2024-11-06 20:22:54 +0200

    Burger fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,71 +12,67 @@ function NavBar() {
   const handleClick = () => setClick(!click);
 
   return (
-    <>
       <nav className={styles.navbar}>
-        <NavLink exact to="/" className={styles.navLogo}>
-            <img src={axIcon} alt="ax2" className={styles.logo}/>
-        </NavLink>
-        <div className={styles.navContainer}>
 
+        <NavLink exact to="/" className={styles.navLogo}>
+          <img src={axIcon} alt="ax2" className={styles.logo}/>
+        </NavLink>
+
+        <div className={styles.navContainer}>
           <ul className={click ? `${styles.navMenu} ${styles.active}` : styles.navMenu}>
             <li className={styles.navItem}>
-              <NavLink
-                exact
-                to="/"
-                className={({ isActive }) =>
-                  classNames(styles.navLinks, { [styles.navLinksActive]: isActive })
-                }
-                onClick={handleClick}
+              <NavLink exact to="/" className={({isActive}) =>
+                  classNames(styles.navLinks, {[styles.navLinksActive]: isActive})
+              }
+                       onClick={handleClick}
               >
                 Home
               </NavLink>
             </li>
             <li className={styles.navItem}>
-              <NavLink
-                exact
-                to="/about"
-                className={({ isActive }) =>
-                  classNames(styles.navLinks, { [styles.navLinksActive]: isActive })
-                }
-                onClick={handleClick}
+              <NavLink exact to="/about" className={({isActive}) =>
+                  classNames(styles.navLinks, {[styles.navLinksActive]: isActive})
+              }
+                       onClick={handleClick}
               >
                 About
               </NavLink>
             </li>
             <li className={styles.navItem}>
               <NavLink
-                exact
-                to="/blog"
-                className={({ isActive }) =>
-                  classNames(styles.navLinks, { [styles.navLinksActive]: isActive })
-                }
-                onClick={handleClick}
+                  exact
+                  to="/blog"
+                  className={({isActive}) =>
+                      classNames(styles.navLinks, {[styles.navLinksActive]: isActive})
+                  }
+                  onClick={handleClick}
               >
                 Api
               </NavLink>
             </li>
             <li className={styles.navItem}>
               <NavLink
-                exact
-                to={isLoggedIn ? "/userdetails" : "/login"}
-                className={({ isActive }) =>
-                  classNames(styles.navLinks, { [styles.navLinksActive]: isActive })
-                }
-                onClick={handleClick}
+                  exact
+                  to={isLoggedIn ? "/userdetails" : "/login"}
+                  className={({isActive}) =>
+                      classNames(styles.navLinks, {[styles.navLinksActive]: isActive})
+                  }
+                  onClick={handleClick}
               >
                 {isLoggedIn ? "Your Account" : "Login"}
               </NavLink>
             </li>
           </ul>
 
-            <NavLink exact to="/" className={styles.navBurger} onClick={handleClick}>
-              <img src={menuIcon} alt="menu" className={styles.navIcon}/>
-            </NavLink>
-
+          {/* Updated burger icon to toggle without redirecting */}
+          <div>
+            <img src={axIcon} alt="ax2" className={styles.navIcon}/>
+          </div>
+          <div className={styles.navBurger} onClick={handleClick}>
+            <img src={menuIcon} alt="menu" />
+          </div>
         </div>
       </nav>
-    </>
   );
 }
 
